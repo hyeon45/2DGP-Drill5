f41639acddc9c0280a21f96321d9d19b93ff6bad
hyeon45 <gahyeon4585@daum.net>
2024-09-30 17:17:45 +0900

    캐릭터의 프레임과 픽셀을 조정 및 배경의 크기 설정, 수업시간에 활용했던 좌우 움직이는 방향을 코딩

---
+++ move_my_character_with_key.py
@@ -0,0 +1,44 @@
+from pico2d import *
+
+open_canvas()
+background = load_image('TUK_GROUND.png')
+character = load_image('download_character_image.png')
+
+running = True
+frame = 0
+x = 800 // 2
+y = 90
+dir = 0
+
+def handle_events():
+    global running, dir
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.key == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
+
+
+while running:
+    clear_canvas()
+    background.draw_to_origin(0, 0, 800, 600)
+    character.clip_draw(frame * 60, 60 * 1, 60, 60, x, y, 100, 100)
+    update_canvas()
+    handle_events()
+    frame = (frame + 1) % 8
+    x += dir * 5
+    delay(0.05)
+
+close_canvas()
