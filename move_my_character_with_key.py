b96fd8ca9f70360afe6b5d55be097759dc5339ce
hyeon45 <gahyeon4585@daum.net>
2024-09-30 17:25:11 +0900

    상하 움직임 확인

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -9,9 +9,10 @@ frame = 0
 x = 800 // 2
 y = 90
 dir = 0
+height = 0
 
 def handle_events():
-    global running, dir
+    global running, dir, height
 
     events = get_events()
     for event in events:
@@ -22,6 +23,10 @@ def handle_events():
                 dir += 1
             elif event.key == SDLK_LEFT:
                 dir -= 1
+            elif event.key == SDLK_UP:
+                height += 1
+            elif event.key == SDLK_DOWN:
+                height -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.key == SDL_KEYUP:
@@ -29,16 +34,25 @@ def handle_events():
                 dir -= 1
             elif event.key == SDLK_LEFT:
                 dir += 1
-
+            elif event.key == SDLK_UP:
+                height -= 1
+            elif event.key == SDLK_DOWN:
+                height += 1
 
 while running:
     clear_canvas()
+
     background.draw_to_origin(0, 0, 800, 600)
     character.clip_draw(frame * 60, 60 * 1, 60, 60, x, y, 100, 100)
+
     update_canvas()
     handle_events()
+
     frame = (frame + 1) % 8
+
     x += dir * 5
+    y += height * 5
+
     delay(0.05)
 
 close_canvas()
